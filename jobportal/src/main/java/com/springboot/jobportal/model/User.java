2efff08ce22dba02cb3e958cffbd1b30adad96a3
Abhiram-rp <abhiramrp081@gmail.com>
2026-05-23 17:05:26 +0530

    Added gtters and setters for User

--- jobportal/src/main/java/com/springboot/jobportal/model/User.java
+++ jobportal/src/main/java/com/springboot/jobportal/model/User.java
@@ -53,5 +53,53 @@ public class User {
         this.role = role;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public AuthProvider getAuthProvider() {
+        return authProvider;
+    }
+
+    public void setAuthProvider(AuthProvider authProvider) {
+        this.authProvider = authProvider;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
     
 }
